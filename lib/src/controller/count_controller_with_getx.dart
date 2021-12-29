02b423a8d72984ac42331faac44a7b3c503c3b07
kicok <kicoker@gmail.com>
2021-12-29 12:39:35 +0900

    add getx 방식 카운트 작업

--- lib/src/controller/count_controller_with_getx.dart
+++ lib/src/controller/count_controller_with_getx.dart
@@ -1,3 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
-class CountControllerWithGetX extends ChangeNotifier {}
+class CountControllerWithGetX extends GetxController {
+  int count = 0;
+  void increase() {
+    count++;
+    update();
+  }
+}
--- lib/src/state/with_getx.dart
+++ lib/src/state/with_getx.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_route_getx/src/controller/count_controller_with_getx.dart';
 import 'package:flutter_sample_route_getx/src/utils/util.dart';
+import 'package:get/get.dart';
 
 class WithGetX extends StatelessWidget {
   const WithGetX({Key? key}) : super(key: key);
@@ -11,10 +13,17 @@ class WithGetX extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text("GetX", style: TextStyle(fontSize: 50)),
-          const Text("0", style: TextStyle(fontSize: 50)),
+          GetBuilder<CountControllerWithGetX>(builder: (controller) {
+            return Text(
+              "${controller.count}",
+              style: const TextStyle(fontSize: 50),
+            );
+          }),
           TextButton(
             style: Utils.textButtonStryle,
-            onPressed: () {},
+            onPressed: () {
+              Get.find<CountControllerWithGetX>().increase();
+            },
             child: const Text(
               "+",
               style: TextStyle(fontSize: 30),
