61a3d3de8fecca57dc899822870e6ecb8100f721
kicok <kicoker@gmail.com>
2022-01-03 17:12:35 +0900

    BindingSamplePge Get.find 와 GetBuilder 작성

--- lib/src/pages/binding_sample_page.dart
+++ lib/src/pages/binding_sample_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_route_getx/src/controller/count_controller_with_getx.dart';
+import 'package:flutter_sample_route_getx/src/utils/util.dart';
+import 'package:get/get.dart';
 
 class BindingSamplePage extends StatelessWidget {
   const BindingSamplePage({Key? key}) : super(key: key);
@@ -7,6 +10,22 @@ class BindingSamplePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          GetBuilder<CountControllerWithGetX>(
+              id: "first",
+              builder: (controller) {
+                return Text(controller.count.toString());
+              }),
+          TextButton(
+            style: Utils.textButtonStryle,
+            onPressed: () {
+              Get.find<CountControllerWithGetX>().increase('first');
+            },
+            child: const Text("+first"),
+          ),
+        ],
+      ),
     );
   }
 }
